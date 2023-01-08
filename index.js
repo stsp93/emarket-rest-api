cc08a76ae6725c4acbd29fac8c5f4275c148b9a9
stsp93 <stspsp@gmail.com>
2023-01-08 11:46:15 +0200

    server set up

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const cors = require('./middlewares/cors');
+const app = express();
 
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({message: 'Working'})
+})
+
+const PORT = process.env.PORT || 3030
+
+app.listen(PORT,()  => console.log(`Listening to port ${PORT}`))
 
