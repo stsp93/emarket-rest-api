97f03c1fcfee3d378057a1ba67264bd958034697
stsp93 <stspsp@gmail.com>
2023-01-20 00:16:45 +0200

    spelling correction

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ const SALT_ROUNDS = 10;
 const JWT_SECRET = 'N*!7y8N&1nyj!Ljw8!w uh1@'
 const CATEGORIES = ['Deals',
     'Clothing',
-    'Electronic',
+    'Electronics',
     'Entertainment',
     'Hobbies',
     'Housing']
