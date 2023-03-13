3ec8ff44151fd4ffc5a80f14983cfba930ac0349
stsp93 <stspsp@gmail.com>
2023-03-13 09:46:24 +0200

    fixed error messages

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,7 +3,7 @@ module.exports = (error) => {
     if (error.code === 11000) {
         const path = Object.keys(error.keyPattern)[0]
         // return { errors: { [path]: path + ' already exist' } }
-        return { errors:  `${path}` + ' already exist' }
+        return { errors:  `${path.slice(0,1).toUpperCase() + path.slice(1)}` + ' already exist' }
 
         // Handling validation errors 
     } else if(error.errors) {
