580180ce61f720b914843882d4894e3bef3beaff
stsp93 <stspsp@gmail.com>
2023-03-13 09:45:10 +0200

    categories rearrange

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,12 @@
 const SALT_ROUNDS = 10;
 const JWT_SECRET = 'N*!7y8N&1nyj!Ljw8!w uh1@'
 const CATEGORIES = {
-    'Vehicles': `https://images.unsplash.com/photo-1630165356623-266076eaceb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`,
-    'Clothing': `https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`,
     'Electronics': `https://images.unsplash.com/photo-1627313433073-a98d074772e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`,
+    'Clothing': `https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`,
+    'Housewares': 'https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
+    'Vehicles': `https://images.unsplash.com/photo-1630165356623-266076eaceb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`,
     'Hobbies': `https://images.unsplash.com/photo-1629055871644-7867e4b11f14?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80`,
     'Housing': `https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80`,
-    'Housewares': 'https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'
 }
 
 const NO_IMG_URL = `https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg`
